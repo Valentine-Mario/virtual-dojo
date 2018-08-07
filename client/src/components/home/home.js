be3deb1277add556ca1396b60ebdbe3d2236c16c
Ojini Chizoba Jude <judoc96@gmail.com>
2018-08-07 05:17:59 +0100

    test simple login, signup using json server and setup 404 error page

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import { API_URL } from '../../config';
 
+class Home extends React.Component {
+  constructor(props){
+    super(props);
 
-const Home = () => {
-  return (
-    <div>
-      Home page
-    </div>
-  )
+    this.state = {
+      data: [],
+      errors: []
+    }
+  }
+
+  componentDidMount(){
+
+    fetch(`${API_URL}/account`, {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          data
+        })
+      },
+      (errors) => {
+        console.log(errors);
+      })
+
+  }
+
+  render(){
+    return (
+      <div>
+        Home page
+      </div>
+    )
+  }
 }
 
 export default Home
--- client/src/index.js
+++ client/src/index.js
@@ -4,22 +4,14 @@ import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 import Home from './components/home/home';
 import Login from './components/login/login';
 import Signup from './components/signup/signup';
+import PersistentDrawer from './components/drawer/drawer';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 const Index = () => {
   return (
     <BrowserRouter>
-      <div>
-        <NavLink to="/" >Home</NavLink>
-        <NavLink to="/login" >Login</NavLink>
-        <NavLink to="/signup" >Signup</NavLink>
-        <Switch>
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </div>
+        <PersistentDrawer />
     </BrowserRouter>
   )
 }
