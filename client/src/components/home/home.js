01152ea431a5e1cbed00629c13ee4cc4f198321a
Ojini Chizoba Jude <judoc96@gmail.com>
2018-08-24 13:48:33 +0100

    home page component adjustment

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -13,11 +13,14 @@ import {
   Loader,
   Button
 } from 'semantic-ui-react';
+import { REQ_GET } from '../../api';
 import AllCourses from '../course/allCourses';
-
 import headerImage from '../../images/header.jpg';
+import content from '../../images/content.jpg';
+import job from '../../images/job.jpg';
+import mentor from '../../images/mentor.jpg';
+import student from '../../images/student.png';
 
-import { REQ_GET } from '../../api'
 
 
 export class Home extends PureComponent {
@@ -76,26 +79,12 @@ export class Home extends PureComponent {
       alignItems: 'center'
     }
 
-    let card = output.map((user) => {
-        return (
-          <Card raised key={user.id} >
-            <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
-            <Card.Content>
-              <Card.Header>{user.author}</Card.Header>
-              <Card.Meta>
-                <span className='date'>{user.date}</span>
-              </Card.Meta>
-              <Card.Description>{user.body}</Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a>
-                <Icon name='user' />
-                {user.title}
-              </a>
-            </Card.Content>
-          </Card>
-        )
-    })
+    const center = {
+      display: 'flex', 
+      alignItems: 'center', 
+      justifyContent: 'center',
+      textAlign: 'center'
+    }
 
     return (
       <Responsive style={{fontFamily: 'Roboto'}}>
@@ -122,7 +111,7 @@ export class Home extends PureComponent {
               <Grid.Column >
                 <Grid.Row>
                   <Grid.Column >
-                    <Header as='h2' style={{color: 'white', marginLeft: '40px', fontSize: '2em', fontWeight: '500', width: '75%'}}>
+                    <Header as='h2' style={{color: 'white', marginLeft: '40px', fontSize: '2em', fontWeight: '500', width: '70%'}}>
                       Learn All You Can
                       <Header.Subheader style={{fontSize: '0.5em', color: 'white', fontWeight: '400'}}>SchoolFlip Offers A Unique Opportunity For You To Learn At Your Own Pace Our Courses are Made For Everyone</Header.Subheader>
                       <Button color='blue' onClick={this.handleSubmit} style={{width: '38%', height: '50px', marginTop: '10px'}} >
@@ -137,26 +126,18 @@ export class Home extends PureComponent {
           </Grid.Row>
         </Grid>
 
-        <Grid columns={1} divided style={{marginTop: '0px'}}>
-          <Grid.Row style={{backgroundColor: 'rgb(255, 255, 255)', height: '100px'}}>
-            <Grid.Column style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%'}}>
-              <Grid.Row>
-                <Grid.Column>
-                  <Header as='h3' textAlign='center' style={{fontWeight: '500'}}>
-                    Testimonial
-                  </Header>
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row>
-                <Grid.Column>
-                  <Header as='h4' textAlign='center' style={{fontWeight: '300'}}>
-                    &quot;I'm constantly amazed by te quality of content provided by this&quot;
-                  </Header>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+        <div>
+          <Header as='h2' textAlign="center" style={{marginTop: '20px', paddingTop: '20px'}}>
+            Testimonial
+            <Header.Subheader style={{padding: '10px', paddingBottom: '0px'}}>I'm constantly amazed by the quality of content provided by this platform</Header.Subheader>
+          </Header>
+          <Grid.Column style={{...center, paddingBottom: '10px'}}>
+            -Student
+          </Grid.Column>
+          <Grid.Column style={center}>
+            <Image src={student} />
+          </Grid.Column>
+        </div>
 
           <Card.Group centered style={{marginTop: '0px', marginBottom: '20px'}}>
 
@@ -173,16 +154,106 @@ export class Home extends PureComponent {
             
           </Card.Group>
 
-            <Grid divided='vertically' style={{padding: '20px', margin: '10px'}} >
-              <Grid.Row columns={2}>
-                <Grid.Column>
-                  <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+          <div>
+            <Header as='h2' textAlign="center" color="blue" style={{marginTop: '10px', paddingTop: '10px'}}>
+              Changing The Way You Learn
+              <Header.Subheader style={{padding: '10px', color: 'black'}}>Master in demand skills, customised learning with personal tutors to help guide you</Header.Subheader>
+            </Header>
+          </div>
+
+          <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+            <Grid style={{padding: '20px', margin: '10px', marginTop: '20px', marginBottom: '20px'}} >
+
+              <Grid.Row columns={2} >
+                <Grid.Column style={center}>
+                  <div>
+                    <Header as='h2' icon textAlign='center' color="blue">
+                      <Icon name='computer' circular />
+                      <Header.Content>High Definition Content</Header.Content>
+                    </Header>
+                    High definition tutorial video, different levels of content ranging from basic to advance
+                  </div>
                 </Grid.Column>
-                <Grid.Column>
-                  <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+                <Grid.Column style={center}>
+                  <Image src={content} rounded />
                 </Grid.Column>
               </Grid.Row>
+
+              <Grid.Row columns={2}>
+                <Grid.Column style={center}>
+                  <Image src={mentor} rounded />
+                </Grid.Column>
+                <Grid.Column style={center}>
+                  <div>
+                    <Header as='h2' icon textAlign='center' color="blue">
+                      <Icon name='users' circular />
+                      <Header.Content>Personal Mentorship</Header.Content>
+                    </Header>
+                    We help ensure your success. You have access to tutors to aid your learning process
+                  </div>
+                </Grid.Column>
+              </Grid.Row>
+
+              <Grid.Row columns={2}>
+                <Grid.Column style={center}>
+                  <div>
+                    <Header as='h2' icon textAlign='center' color="blue">
+                      <Icon name='briefcase' circular />
+                      <Header.Content>Job Opportunity</Header.Content>
+                    </Header>
+                    Begin the journey to your life and career goals. We are affiliated with talent scouts from top industries
+                  </div>
+                </Grid.Column>
+                <Grid.Column style={center}>
+                  <Image src={job} rounded />
+                </Grid.Column>
+              </Grid.Row>
+
             </Grid>
+          </Responsive>
+
+          <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
+              <Grid style={{padding: '20px', margin: '10px', marginTop: '20px', marginBottom: '20px'}} >
+
+                <Grid.Row columns={1} >
+                  <Grid.Column style={center}>
+                    <div>
+                      <Header as='h2' icon textAlign='center' color="blue">
+                        <Icon name='computer' circular />
+                        <Header.Content>High Definition Content</Header.Content>
+                      </Header>
+                      High definition tutorial video, different levels of content ranging from basic to advance
+                    </div>
+                  </Grid.Column>
+                </Grid.Row>
+
+                <Grid.Row columns={1}>
+                  <Grid.Column style={center}>
+                    <div>
+                      <Header as='h2' icon textAlign='center' color="blue">
+                        <Icon name='users' circular />
+                        <Header.Content>Personal Mentorship</Header.Content>
+                      </Header>
+                      We help ensure your success. You have access to tutors to aid your learning process
+                    </div>
+                  </Grid.Column>
+                </Grid.Row>
+
+                <Grid.Row columns={1}>
+                  <Grid.Column style={center}>
+                    <div>
+                      <Header as='h2' icon textAlign='center' color="blue">
+                        <Icon name='briefcase' circular />
+                        <Header.Content>Job Opportunity</Header.Content>
+                      </Header>
+                      Begin the journey to your life and career goals. We are affiliated with talent scouts from top industries
+                    </div>
+                  </Grid.Column>
+                </Grid.Row>
+
+              </Grid>
+          </Responsive>
+
 
       </Responsive>
     )
@@ -190,5 +261,3 @@ export class Home extends PureComponent {
 }
 
 export default withRouter(Home);
-
-
