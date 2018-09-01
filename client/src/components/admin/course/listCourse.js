859fb2b4c297f126bb19728a4fbe1a170f2ecfe8
Ojini Chizoba Jude <judoc96@gmail.com>
2018-09-01 19:24:24 +0100

    only admin can delete any course and any video in a particular course

--- client/src/components/admin/course/listCourse.js
+++ client/src/components/admin/course/listCourse.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Table, Dimmer, Loader, Image, Icon } from 'semantic-ui-react';
-import { REQ_GET } from '../../../api';
+import { Button, Table, Dimmer, Loader, Image, Icon, Grid, List } from 'semantic-ui-react';
+import { REQ_GET, REQ_POST } from '../../../api';
 import MainNav from '../menu/mainNav';
 import SideNav from '../menu/sideNav';
 
@@ -11,7 +11,10 @@ class ListCourses extends Component {
 
         this.state = {
         	courses: [],
-        	loading: false
+        	courseVideo: [],
+        	loading: false,
+        	deleting: false,
+        	loadVideo: false
         }
     }
 
@@ -20,11 +23,14 @@ class ListCourses extends Component {
     		loading: true
     	})
 
+    	this.getAllCourses();
+    }
+
+    getAllCourses = () => {
     	try {
     		// statements
 	    	REQ_GET('category/get')
 	    		.then(res => {
-	    			console.log(res);
 	    			this.setState({
 	    				courses: res.data,
 	    				loading: false
@@ -42,12 +48,18 @@ class ListCourses extends Component {
 
     handleCourseVideo = (id) => {
     	console.log('clicked');
+    	this.setState({
+    		loadVideo: true
+    	})
 
     	try {
     		// statements
 	    	REQ_GET(`category/get/${id}`)
 	        .then(res => {
-	          console.log(res.data);
+	          this.setState({
+	          	courseVideo: res.data.videos,
+	          	loadVideo: false
+	          })
 	        });
     	} catch(e) {
     		// statements
@@ -57,11 +69,22 @@ class ListCourses extends Component {
     }
 
     handleCourseDelete = (id) => {
+    	let userid = JSON.parse(localStorage.getItem('user'));
+    	this.setState({
+    		deleting: true
+    	})
+
     	try {
     		// statements
-	    	REQ_GET(`category/delete/${id}`)
+	    	REQ_POST(`category/delete/${id}`, {user: userid[0]})
 	        .then(res => {
-	          window.location.reload();
+	          // window.location.reload();
+	          this.getAllCourses();
+
+	          this.setState({
+	          	deleting: false
+	          })
+
 	        });
     	} catch(e) {
     		// statements
@@ -70,8 +93,24 @@ class ListCourses extends Component {
 
     }
 
+    handleVideoDelete = (id) => {
+    	let userid = JSON.parse(localStorage.getItem('user'));
+
+    	try {
+    		// statements
+	    	REQ_POST(`video/delete/${id}`, {user: userid[0]})
+	    		.then(res => {
+	    			this.handleCourseVideo(id);
+	    		})
+    	} catch(e) {
+    		// statements
+    		console.log(e);
+    	}
+
+    }
+
     render() {
-    	let { courses, loading } = this.state;
+    	let { courses, loading, deleting, courseVideo, loadVideo } = this.state;
 
         return (
         	<div>
@@ -84,37 +123,70 @@ class ListCourses extends Component {
 		            <Dimmer active={loading} inverted>
 		                <Loader indeterminate>Getting Courses</Loader>
 		            </Dimmer>
+		            <Dimmer active={deleting} inverted>
+		                <Loader indeterminate>Deleting Course</Loader>
+		            </Dimmer>
 
-		            <Table color='blue' structured selectable style={{width: '65%'}} celled striped>
-				        <Table.Header>
-				          <Table.Row>
-				            <Table.HeaderCell>Image</Table.HeaderCell>
-				            <Table.HeaderCell>Name</Table.HeaderCell>
-				            <Table.HeaderCell>Description</Table.HeaderCell>
-				            <Table.HeaderCell></Table.HeaderCell>
-				          </Table.Row>
-				        </Table.Header>
 
-				        <Table.Body>
 
-				        {
-				        	courses.map((course) => {
-				        		return (
-						          <Table.Row key={course._id} style={{cursor: 'pointer'}} >
-						            <Table.Cell onClick={() => this.handleCourseVideo(course._id)}>
-						            	<Image src={course.image} size="tiny" />
-						            </Table.Cell>
-						            <Table.Cell onClick={() => this.handleCourseVideo(course._id)}>{course.name}</Table.Cell>
-						            <Table.Cell>{course.description}</Table.Cell>
-						            <Table.Cell onClick={() => this.handleCourseDelete(course._id)}>
-						            	<Icon name="trash alternate" size="big" color="red" />
-						            </Table.Cell>
-						          </Table.Row>
-				        		)
-				        	})
-				        }
-				        </Table.Body>
-				    </Table>
+		            <Grid columns={2}>
+				      <Grid.Column width={10}>
+				        
+			            <Table color='blue' structured selectable style={{width: '100%', marginTop: '10px'}} celled striped>
+					        <Table.Header>
+					          <Table.Row>
+					            <Table.HeaderCell>Image</Table.HeaderCell>
+					            <Table.HeaderCell>Name</Table.HeaderCell>
+					            <Table.HeaderCell>Description</Table.HeaderCell>
+					            <Table.HeaderCell></Table.HeaderCell>
+					          </Table.Row>
+					        </Table.Header>
+
+					        <Table.Body>
+
+					        {
+					        	courses &&
+						        	courses.map((course) => {
+						        		return (
+								          <Table.Row key={course._id} style={{cursor: 'pointer'}} >
+								            <Table.Cell onClick={() => this.handleCourseVideo(course._id)}>
+								            	<Image src={course.image} size="tiny" />
+								            </Table.Cell>
+								            <Table.Cell onClick={() => this.handleCourseVideo(course._id)}>{course.name}</Table.Cell>
+								            <Table.Cell>{course.description}</Table.Cell>
+								            <Table.Cell onClick={() => this.handleCourseDelete(course._id)}>
+								            	<Icon name="trash alternate" size="big" color="red" />
+								            </Table.Cell>
+								          </Table.Row>
+						        		)
+						        	})
+					        }
+					        </Table.Body>
+					    </Table>
+				        
+				      </Grid.Column>
+				      <Grid.Column width={6}>
+				      	<Dimmer active={loadVideo} inverted>
+				      		<Loader indeterminate >Getting Videos</Loader>
+				      	</Dimmer>
+				        <List divided verticalAlign='middle'>
+						    {
+						    	courseVideo &&
+						    		courseVideo.map((video) => {
+						    			return (
+						    				<List.Item key={video._id}>
+										      <List.Content floated='right'>
+										        <Icon style={{cursor: 'pointer'}} name="trash" color="red" onClick={() => this.handleVideoDelete(video._id)} />
+										      </List.Content>
+										      <Icon name='video play' size="big" />
+										      <List.Content>{video.name}</List.Content>
+										    </List.Item>
+						    			)
+						    		})
+						    }
+						</List>
+				      </Grid.Column>
+				    </Grid>
 	            </div>
 	            <SideNav />
             </div>
